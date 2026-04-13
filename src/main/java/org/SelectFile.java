916ba919d739eb71e6e96c3f8c247b796a1cd2fc
witchcrafttwo <bsi25009@uhe.ac.jp>
2026-04-13 11:16:23 +0900

    gradle javafxによるGUI開発に変更

--- src/main/java/org/SelectFile.java
+++ src/main/java/org/SelectFile.java
@@ -2,17 +2,34 @@ package org;
 
 import javax.swing.*;
 import java.io.File;
-public class SelectFile {
+import java.util.List;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.*;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+public class SelectFile extends Application {
+    public void start(Stage stage){
+        Application.launch(SelectFile.class);
+
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setTitle( "ディレクトリ選択" );
+        String base = System.getProperty("user.home");
+        directoryChooser.setInitialDirectory( new File(base,"Downloads") );
+        File f4 = directoryChooser.showDialog( stage );
+    }
     public  File Filechooser(int modeselect){
         String Title = null;
         JFileChooser chooser = new JFileChooser("C:\\Users\\yunre\\Downloads");
         File Filename = new File(System.getProperty("user.dir")); //現在のディレクトリが初期値のはず
         //ディレクトリ選択のみ
         if(modeselect == 1){
-            Title = ("監視するディレクトリを選択してくれい");
+            Title = ("監視するディレクトリを選択してください");
         }
         if(modeselect == 2){
-            Title = ("移動させるディレクトリを選択してくれい");
+            Title = ("移動させるディレクトリを選択してください");
         }
         try {
             chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
